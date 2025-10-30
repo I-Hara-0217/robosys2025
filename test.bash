c69e994ca5f2b82579804f77fbab6d3ea4fd318b
I.Hara <i.hara.git@gmail.com>
2025-10-30 13:13:05 +0900

    テスト項目の追加

--- test.bash
+++ test.bash
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/bash -xv
+#SPDX-FileCopyrightText: 2025 Ibuki Hara
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
 	echo ${1}行目が違うよ
@@ -6,8 +8,20 @@ ng () {
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
+
+### NORMAL INPUT ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)           #計算できない値を入力してみる
+[ "$?" = 1 ]      || ng "$LINENO" #終了ステータスが1なのを確認
+[ "${out}" = 15 ] || ng "$LINENO" #この行と上の行は入れ替えるとダメです
+                                        #（↑なぜかは考えてみましょう）
+out=$(echo | ./plus)              #なにも入力しない
+[ "$?" = 1 ]      || ng "$LINENO" #これも異常終了する
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0] && echo OK
 
 exit $res
