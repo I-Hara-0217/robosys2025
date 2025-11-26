c271ee894f7eb4cee6a5d805fa23c9933c856afe
I.Hara <i.hara.git@gmail.com>
2025-11-26 11:47:26 +0900

    test.bashの更新

--- test.bash
+++ test.bash
@@ -1,10 +1,10 @@
 #!/bin/bash -xv
-#SPDX-FileCopyrightText: 2025 Ibuki Hara
+# SPDX-FileCopyrightText: 2025 Ibuki Hara
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo ${1}行目が違うよ
-	res=1
+    echo ${1}行目が違うよ
+    res=1
 }
 
 res=0
@@ -15,11 +15,22 @@ out=$(echo "test" | ./mdline)
 
 ### STRANGE INPUT ###
 out=$(echo "あ" | ./mdline)
-[ "${out}" = "- あ" ] || ng "$LINENO" 
-                                      
-out=$(echo | ./mdline)              
-[ "$?" = 0 ]      || ng "$LINENO" 
+[ "${out}" = "- あ" ] || ng "$LINENO"
+
+### EMPTY INPUT ###
+out=$(echo | ./mdline)
+[ "$?" = 0 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
+
+### OPTION TEST ###
+out=$(echo -e "first\nsecond" | ./mdline -n)
+expected="1. first
+2. second"
+
+[ "${out}" = "${expected}" ] || ng "$LINENO"
+
+out=$(echo "todo" | ./mdline -c)
+[ "${out}" = "- [ ] todo" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 
